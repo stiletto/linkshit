c3e150b998e1549a9432204c27b9c286dab556b3
Stiletto <blasux@blasux.ru>
2014-09-09 15:53:31 +0400

    Made the library proper python package

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='linkshit',
+    version='0.1',
+    description='Library for finding of links in text and parsing simple markups',
+    author='Stiletto',
+    license='MIT',
+    author_email='blasux@blasux.ru',
+    url='http://github.com/stiletto/linkshit',
+    packages=['linkshit'],
+)
